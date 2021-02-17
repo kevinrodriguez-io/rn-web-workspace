f62b4e7ad92ba0adc0e810664fa6149bf62a3066
Kevin Rodriguez <_@kevinrodriguez.io>
2021-02-17 17:16:08 -0600

    feat: some small changes for loading fonts

---
+++ apps/mobile/src/hooks/useLoadFonts.ts
@@ -0,0 +1,33 @@
+import { loadAsync } from 'expo-font'
+import { useEffect, useState } from 'react'
+
+const useLoadFonts = () => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        await loadAsync({
+          'OpenSans-Light': require('../assets/fonts/OpenSans-Light.ttf'),
+          'OpenSans-LightItalic': require('../assets/fonts/OpenSans-LightItalic.ttf'),
+          'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+          'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
+          'OpenSans-SemiBold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
+          'OpenSans-SemiBoldItalic': require('../assets/fonts/OpenSans-SemiBoldItalic.ttf'),
+          'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+          'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf'),
+          'OpenSans-ExtraBold': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
+          'OpenSans-ExtraBoldItalic': require('../assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
+        })
+      } catch (error) {
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, [setLoading, setError])
+  return [loading, error]
+}
+
+export default useLoadFonts
--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -1,34 +1,16 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import { loadAsync } from 'expo-font'
 import ButtonsContainer from 'components/ButtonsContainer'
+import useLoadFonts from 'hooks/useLoadFonts'
 
 const App: FC = () => {
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    ;(async () => {
-      setLoading(true)
-      try {
-        await loadAsync({
-          'OpenSans-Light': require('../assets/fonts/OpenSans-Light.ttf'),
-          'OpenSans-LightItalic': require('../assets/fonts/OpenSans-LightItalic.ttf'),
-          'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
-          'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
-          'OpenSans-SemiBold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
-          'OpenSans-SemiBoldItalic': require('../assets/fonts/OpenSans-SemiBoldItalic.ttf'),
-          'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-          'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf'),
-          'OpenSans-ExtraBold': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
-          'OpenSans-ExtraBoldItalic': require('../assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
-        })
-      } catch (error) {
-        alert(error)
-      } finally {
-        setLoading(false)
-      }
-    })()
-  }, [])
-  if (loading)
+  /**
+   * Note: This is only needed for expo to recognize app fonts
+   * This can be removed in a production build to save some initial
+   * loading time.
+   */
+  const [loadingFonts] = useLoadFonts()
+  if (loadingFonts)
     return (
       <View style={styles.container}>
         <ActivityIndicator />
